e9fd18c86771e9badfb354afb7570e88c9b551ce
KaykyRenner <kayky.renner.sg@gmail.com>
2024-08-04 16:46:44 -0300

    04/08

--- assets/js/arquivo.js
+++ assets/js/arquivo.js
@@ -13,93 +13,70 @@ function addLiEmUL(comida){
   li.setAttribute('class', 'ListaDeComida')
   ul.appendChild(li)
   li.innerHTML = comida
-}
+}}
+function EsconderEMostrarButton(comprar, add){
+  const comprar1 = document.querySelector(comprar)
+  const add1 = document.querySelector(add)
+  if(comprar)add1.style.display = 'inline-block'
+  if(add)comprar1.style.display = 'none'
 }
 document.addEventListener('click', e =>{
   const el = e.target;
   const item = document.querySelector('.item')
   if(el.classList.contains('comprar1')){
-    const comprar1 = document.querySelector('.comprar1')
-    const add1 = document.querySelector('.add1')
-    add1.style.display = 'inline-block'
-    comprar1.style.display = 'none'
+    EsconderEMostrarButton('.comprar1','.add1')
     addLiEmUL('Waffle with Berries')
     valorItem+=6.50
     contador++
   }
   if(el.classList.contains('comprar2')){
-    const comprar1 = document.querySelector('.comprar2')
-    const add1 = document.querySelector('.add2')
-    add1.style.display = 'inline-block'
-    comprar1.style.display = 'none'
+    EsconderEMostrarButton('.comprar2','.add2')
     addLiEmUL(`Vanilla Bean Crème Brûlée`)
     valorItem+=7.00
     contador++
 
   }
   if(el.classList.contains('comprar3')){
-    const comprar1 = document.querySelector('.comprar3')
-    const add1 = document.querySelector('.add3')
-    add1.style.display = 'inline-block'
-    comprar1.style.display = 'none'
+    EsconderEMostrarButton('.comprar3','.add3')
     addLiEmUL('Macaron Mix of Five')
     valorItem+=8.00
     contador++
   }
   if(el.classList.contains('comprar4')){
-    const comprar1 = document.querySelector('.comprar4')
-    const add1 = document.querySelector('.add4')
-    add1.style.display = 'inline-block'
-    comprar1.style.display = 'none'
+    EsconderEMostrarButton('.comprar4','.add4')
     addLiEmUL('Classic Tiramisu')
     valorItem+=5.50
     contador++
   }
   if(el.classList.contains('comprar5')){
-    const comprar1 = document.querySelector('.comprar5')
-    const add1 = document.querySelector('.add5')
-    add1.style.display = 'inline-block'
-    comprar1.style.display = 'none'
+    EsconderEMostrarButton('.comprar5','.add5')
     addLiEmUL('Pistachio Baklava')
     valorItem+=4.00
     contador++
   }
   if(el.classList.contains('comprar6')){
-    const comprar1 = document.querySelector('.comprar6')
-    const add1 = document.querySelector('.add6')
-    add1.style.display = 'inline-block'
-    comprar1.style.display = 'none'
+    EsconderEMostrarButton('.comprar6','.add6')
     addLiEmUL('Lemon Meringue Pie')
     valorItem+=5.00
     contador++
   }
   if(el.classList.contains('comprar7')){
-    const comprar1 = document.querySelector('.comprar7')
-    const add1 = document.querySelector('.add7')
-    add1.style.display = 'inline-block'
-    comprar1.style.display = 'none'
+    EsconderEMostrarButton('.comprar7','.add7')
     addLiEmUL('Red Velvet Cake')
     valorItem+=4.50
     contador++
   }
   if(el.classList.contains('comprar8')){
-    const comprar1 = document.querySelector('.comprar8')
-    const add1 = document.querySelector('.add8')
-    add1.style.display = 'inline-block'
-    comprar1.style.display = 'none'
+    EsconderEMostrarButton('.comprar8','.add8')
     addLiEmUL('Salted Caramel Brownie')
     valorItem+=4.50
     contador++
   }
   if(el.classList.contains('comprar9')){
-    const comprar1 = document.querySelector('.comprar9')
-    const add1 = document.querySelector('.add9')
-    add1.style.display = 'inline-block'
-    comprar1.style.display = 'none'
+    EsconderEMostrarButton('.comprar9','.add9')
     addLiEmUL('Vanilla Panna Cotta')
     valorItem+=6.50
     contador++
   }
   item.innerHTML =`Your Cart(${contador}), total:$${valorItem.toFixed(2)}`
 })
-
